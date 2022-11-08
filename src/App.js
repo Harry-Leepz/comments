1da874132f12fd922ef7f5fbf40c25aca24842b9
Harry Dhillon <hdhillon478@gmail.com>
2022-11-08 20:10:14 +0000

    Add components to routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route path='/quotes'></Route>
-        <Route path='/quotes/:quoteId'></Route>
-        <Route path='/new-quote'></Route>
+        <Route path='/quotes'>
+          <AllQuotes />
+        </Route>
+        <Route path='/quotes/:quoteId'>
+          <QuoteDetail />
+        </Route>
+        <Route path='/new-quote'>
+          <NewQuote />
+        </Route>
       </Switch>
     </div>
   );
