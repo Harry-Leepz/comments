fcbc20b74d2d9440ba9837db4c66d403673dcf65
Harry Dhillon <hdhillon478@gmail.com>
2022-11-08 20:16:41 +0000

    Add redirect route to AllQuotes component page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
@@ -7,7 +7,10 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route path='/quotes'>
+        <Route path='/' exact>
+          <Redirect to='/quotes' />
+        </Route>
+        <Route path='/quotes' exact>
           <AllQuotes />
         </Route>
         <Route path='/quotes/:quoteId'>
