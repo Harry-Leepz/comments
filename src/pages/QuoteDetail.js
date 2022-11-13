df001a9950bd96ec784752ab94f62c2cac0bae4b
Harry Dhillon <hdhillon478@gmail.com>
2022-11-13 19:26:56 +0000

    Add nested route for Comments component

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,5 @@
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
   const params = useParams();
@@ -7,6 +8,9 @@ const QuoteDetail = () => {
     <div>
       Quote Detail Page
       <h1>{params.quoteId}</h1>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </div>
   );
 };
