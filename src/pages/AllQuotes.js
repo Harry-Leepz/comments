f757630f5b7a4062100b2b427095890332cd9940
Harry Dhillon <hdhillon478@gmail.com>
2022-11-15 13:58:44 +0000

    Add DUMMY_DATA to AllQuotes component

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,4 +1,20 @@
-import React from "react";
+const DUMMY_DATA = [
+  {
+    id: 1,
+    author: "Max",
+    text: "Learning React is fun",
+  },
+  {
+    id: 2,
+    author: "Harry",
+    text: "Learning Python is great",
+  },
+  {
+    id: 3,
+    author: "Jeevan",
+    text: "Learning .Net is awful",
+  },
+];
 
 const AllQuotes = () => {
   return <div>AllQuotes</div>;
