77ea57db3a2f7b7a61b8468cbec06c42a49a27e5
Harry Dhillon <hdhillon478@gmail.com>
2022-11-16 21:51:53 +0000

    Coopy and add DUMMY_DATA to QuoteDetail component

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,8 +1,28 @@
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 
+const DUMMY_DATA = [
+  {
+    id: 1,
+    author: "Max",
+    text: "Learning React is fun",
+  },
+  {
+    id: 2,
+    author: "Harry",
+    text: "Learning Python is great",
+  },
+  {
+    id: 3,
+    author: "Jeevan",
+    text: "Learning .Net is awful",
+  },
+];
+
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
 
   return (
     <div>
