206c00b1856963c5995025ce3a43ac5eac012f26
Harry Dhillon <hdhillon478@gmail.com>
2022-11-20 21:08:50 +0000

    Add logic to sort QuoteList dynamically using query params

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,16 +1,45 @@
 import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
 
+// helper function to sort quotes by id's
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  // extract params from the url
+  const queryParams = new URLSearchParams(location.search);
+  // boolean value to check params
+  const isSortingAscending = queryParams.get("sort") === "asc";
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  // re-render page by updating url dynamically and sorting quotes by id
+  const changeSortingHandler = () => {
+    history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
+  };
+
   return (
     <Fragment>
       <div className={classes.sorting}>
-        <button>Sor Ascending</button>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
       </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
